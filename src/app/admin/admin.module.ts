911dd9d2622dc087b6c4499719d2077db37b2f2a
Azim Momin <ayyem@protonmail.com>
2024-01-25 16:43:58 -0500

    Moved http service dependency to app module.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { DonutListComponent } from './containers/donut-list/donut-list.component';
 import { DonutSingleComponent } from './containers/donut-single/donut-single.component';
 
 import { DonutCardComponent } from './components/donut-card/donut-card.component';
 import { DonutFormComponent } from './components/donut-form/donut-form.component';
+import { RouterModule, Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: 'donuts', component: DonutListComponent },
+  { path: 'donut', component: DonutSingleComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'donuts' },
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +22,6 @@ import { DonutFormComponent } from './components/donut-form/donut-form.component
     DonutCardComponent,
     DonutFormComponent,
   ],
-  imports: [CommonModule, FormsModule, HttpClientModule],
-  exports: [DonutListComponent, DonutSingleComponent, DonutCardComponent],
+  imports: [CommonModule, FormsModule, RouterModule.forChild(routes)],
 })
 export class AdminModule {}
